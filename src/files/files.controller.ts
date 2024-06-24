9c33f85f168cc07967db724f60983b98a001cf95
Vira Voronova <vera.voronova@hotmail.com>
2024-06-24 12:28:51 +0300

    file upload swagger config added

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,13 +6,39 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
+import { ApiTags, ApiBearerAuth, ApiConsumes, ApiBody, ApiCreatedResponse, ApiUnauthorizedResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 
+@ApiTags('Files')
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload')
+  @ApiBearerAuth()
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiCreatedResponse({
+    description: 'The file has been successfully uploaded.',
+    schema: {
+      type: 'object',
+      properties: {
+        url: { type: 'string' },
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     const fileUrl = await this.filesService.uploadFile(file);
     return { url: fileUrl };
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { bucket } from 'src/config/firebase/firebase.config';
 import { v4 as uuidv4 } from 'uuid';
-import * as admin from 'firebase-admin';
 
 @Injectable()
 export class FilesService {
@@ -15,48 +13,7 @@ export class FilesService {
       public: true,
     });
 
-    const url = fileUpload.publicUrl();
-    return url;
-
-    // console.log('fileUpload - from firebase', fileUpload);
-    // const blobStream = fileUpload.createWriteStream({
-    //   metadata: {
-    //     contentType: file.mimetype,
-    //   },
-    // });
-
-    // console.log('blobStream', blobStream);
-    // return new Promise((resolve, reject) => {
-    //   blobStream.on('error', error => reject(error));
-    //   blobStream.on('finish', () => {
-    //     fileUpload
-    //       .getSignedUrl({
-    //         action: 'read',
-    //         expires: '03-09-2491', // Set the expiration date far in the future
-    //       })
-    //       .then(signedUrls => {
-    //         resolve(signedUrls[0]);
-    //       });
-    //   });
-    //   blobStream.end(file.buffer);
-    // });
+    return fileUpload.publicUrl();
   }
 
-  // constructor(
-  //   @Inject('FIREBASE_ADMIN') private readonly firebaseAdmin: admin.app.App,
-  // ) {}
-
-  // async uploadFile(file: Express.Multer.File): Promise<string> {
-  //   const bucket = this.firebaseAdmin.storage().bucket();
-  //   const fileName = `uploads/${Date.now()}-${file.originalname}`;
-  //   const fileUpload = bucket.file(fileName);
-
-  //   await fileUpload.save(file.buffer, {
-  //     metadata: { contentType: file.mimetype },
-  //     public: true,
-  //   });
-
-  //   const url = fileUpload.publicUrl();
-  //   return url;
-  // }
 }
